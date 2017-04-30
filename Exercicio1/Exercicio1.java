1c57d9a815be014c3960fcc24d6a6da148401273
thiagomartendal <thiagomartendal@gmail.com>
2017-04-30 19:37:47 -0300

    Update Exercicio1.java

--- Exercicio1/Exercicio1.java
+++ Exercicio1/Exercicio1.java
@@ -1,6 +1,6 @@
 package Exercicio1;
 
-public class Exercicio1{
+public class Exercicio1 {
     public static void main(String[] args){
         Media media = new Media();
         media.setProva1(5);
