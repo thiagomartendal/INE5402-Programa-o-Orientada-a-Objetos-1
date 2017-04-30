ece20a8832dd81e2d081d017ebbabc3d6c422d48
thiagomartendal <thiagomartendal@gmail.com>
2017-04-30 19:11:06 -0300

    Create Exercicio1.java

---
+++ Exercicio1/Exercicio1.java
@@ -0,0 +1,12 @@
+package exercicio1;
+
+public class Exercicio1{
+    public static void main(String[] args){
+        int p1 = 5;
+        int p2 = 3;
+        int tp1 = 10;
+        int tp2 = 8;
+        double ms = (p1*0.3)+(p2*0.4)+(tp1*0.1)+(tp2*0.2);
+        System.out.println("Média: "+ms);
+    }
+}
