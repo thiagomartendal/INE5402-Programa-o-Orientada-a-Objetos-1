3ef967264266cc7b284978750118276ebb11ee49
thiagomartendal <thiagomartendal@gmail.com>
2017-05-01 18:47:31 -0300

    Add files via upload

---
+++ circulo/Circulo.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Circulo{
+	public static void main(String[] args){
+		String num = String.valueOf(JOptionPane.showInputDialog("Digite um número: "));
+	}
+}
